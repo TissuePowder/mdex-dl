70881ee1c37239b31c7c979840ad6415d96aca36
TissuePowder <nillyhan.xt@gmail.com>
2023-07-24 14:38:40 +0600

    Check hash of response body before writing to file

--- chapter.go
+++ chapter.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"crypto/sha256"
 	"encoding/hex"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"net/http"
@@ -31,23 +33,16 @@ type Image struct {
 	Idx  int
 }
 
-func CalcHash(filePath string) (string, error) {
+func CalcSha256(data []byte) (string, error) {
 
-	file, err := os.Open(filePath)
+	hash := sha256.New()
+	_, err := hash.Write(data)
 	if err != nil {
 		return "", err
 	}
-	defer file.Close()
-
-	hash := sha256.New()
-	if _, err := io.Copy(hash, file); err != nil {
-		return "", err
-	}
-
-	hashInBytes := hash.Sum(nil)
-	hashString := hex.EncodeToString(hashInBytes)
-
-	return hashString, nil
+	digest := hex.EncodeToString(hash.Sum(nil))
+	// fmt.Println("digest:", digest)
+	return digest, nil
 }
 
 func DownloadPage(image Image, client *MdClient) error {
@@ -68,26 +63,44 @@ func DownloadPage(image Image, client *MdClient) error {
 	origHash := strings.Split(withoutExt, "-")[1]
 
 	if _, err := os.Stat(filename); os.IsNotExist(err) {
+
 		err := os.MkdirAll(filepath.Dir(filename), 0775)
 		if err != nil {
 			fmt.Println(err.Error())
 			return err
 		}
+
 	} else {
-		existHash, err := CalcHash(filename)
+
+		file, err := os.Open(filename)
 		if err != nil {
 			fmt.Println(err.Error())
 			return err
 		}
-		if origHash == existHash {
+		defer file.Close()
+
+		fileContents, err := io.ReadAll(file)
+		if err != nil {
+			fmt.Println(err.Error())
+			return err
+		}
+
+		fileHash, err := CalcSha256(fileContents)
+		if err != nil {
+			fmt.Println(err.Error())
+			return err
+		}
+
+		if origHash == fileHash {
 			return nil
 		} else {
-			fmt.Println("hash mismatch")
 			err := os.Rename(filename, filename+".bak")
 			if err != nil {
 				return err
 			}
+			fmt.Println("Hash mismatch. Old file is renamed with .bak extension.")
 		}
+
 	}
 
 	file, err := os.Create(filename)
@@ -95,35 +108,51 @@ func DownloadPage(image Image, client *MdClient) error {
 		fmt.Println(err.Error())
 		return err
 	}
+	defer file.Close()
 
-	_, err = io.Copy(file, res.Body)
-
+	var resbuf bytes.Buffer
+	_, err = resbuf.ReadFrom(res.Body)
 	if err != nil {
 		fmt.Println(err.Error())
 		return err
 	}
-	file.Close()
 
-	existHash, err := CalcHash(filename)
+	responseHash, err := CalcSha256(resbuf.Bytes())
 	if err != nil {
 		fmt.Println(err.Error())
 		return err
 	}
-	if origHash == existHash {
+
+	if origHash == responseHash {
+
+		_, err := io.Copy(file, &resbuf)
+		if err != nil {
+			fmt.Println(err.Error())
+			return err
+		}
+
 		fmt.Println(filename)
+
 		return nil
-	} else {
-		fmt.Println("hash mismatch")
-		fmt.Println(filename)
-		os.Exit(1)
 	}
-	return nil
+
+	return errors.New("invalid response received")
 }
 
 func Worker(id int, jobs <-chan Image, results chan<- error, client *MdClient, wg *sync.WaitGroup) {
 	defer wg.Done()
+
+	delay := [3]int{2, 5, 10}
+
 	for job := range jobs {
-		err := DownloadPage(job, client)
+		var err error
+		for retry := 0; retry < 3; retry++ {
+			err = DownloadPage(job, client)
+			if err == nil {
+				break
+			}
+			time.Sleep(time.Second * time.Duration(delay[retry]))
+		}
 		results <- err
 	}
 
